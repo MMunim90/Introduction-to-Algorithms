5a4226e0c80d954183605dbc84660b4b4d452136
MMunim <mmunim9.9.01@gmail.com>
2026-02-15 01:40:26 +0600

    explore dfs

---
+++ Module 3/dfs.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> adj_list[1005];
+bool visited[1005];
+
+void dfs(int src)
+{
+    cout << src << " ";
+    visited[src] = true;
+
+    for(int child : adj_list[src])
+        if(visited[child] == false)
+            dfs(child);
+}
+
+int main()
+{
+    int n, e; cin >> n >> e;
+    while (e--)
+    {
+        int a, b; cin >> a >> b;
+
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
+    }
+    
+    memset(visited, false, sizeof(visited));
+
+    int src; cin >> src;
+    dfs(src);
+    return 0;
+}
