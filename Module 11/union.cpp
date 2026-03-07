5bd4d2cfae8a86b7cc93e31274d74e1a82cb84e1
MMunim <mmunim9.9.01@gmail.com>
2026-03-07 20:12:02 +0600

    union set

---
+++ Module 11/union.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int leader[1005];
+int group_size[1005];
+
+int find(int node)
+{
+    if(leader[node] == -1) return node;
+
+    int lead = find(leader[node]);
+    leader[node] = lead;
+    return lead;
+}
+
+void dsu_union(int node1, int node2)
+{
+    int lead1 = find(node1);
+    int lead2 = find(node2);
+
+    if(group_size[lead1] >= group_size[lead2])
+    {
+        leader[lead2] = lead1;
+        group_size[lead1] += group_size[lead2];
+    }
+    else
+    {
+        leader[lead1] = lead2;
+        group_size[lead2] += group_size[lead1];
+    }
+}
+
+int main()
+{
+    memset(leader, -1, sizeof(leader));
+    memset(group_size, 1, sizeof(group_size));
+
+    dsu_union(1, 2);
+    dsu_union(2, 0);
+    dsu_union(3, 1);
+
+    for(int i=0; i<6; i++)
+        cout << i << " -> " << leader[i] << endl;
+    return 0;
+}
